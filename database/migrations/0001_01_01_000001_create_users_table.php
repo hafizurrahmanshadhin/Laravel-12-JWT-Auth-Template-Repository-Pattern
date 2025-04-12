a570521198753bcf7c94ff99c662230b524a64ff
mdiktushar <md.tushar1804@gmail.com>
2025-04-12 17:53:52 +0600

    migration update

--- database/migrations/0001_01_01_000001_create_users_table.php
+++ database/migrations/0001_01_01_000001_create_users_table.php
@@ -13,11 +13,24 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+
+            $table->string('handle')->unique();
+
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
+
             $table->string('password');
+
+            $table->string('avatar')->nullable();
+
+            $table->foreignId('role_id')->constrained('roles')->cascadeOnDelete();
+
+            $table->boolean('status')->default(true);
+
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
 
